1166d6f424e7a17136e66bb918efab467db78d78
Vicky Nguyen <ztieu.linhz@gmail.com>
2022-01-04 23:04:34 +0700

    tam nhin animation

--- js/app.js
+++ js/app.js
@@ -11,7 +11,18 @@ $(".feedback__main--box").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   prevArrow: `<span class="btn-arrow btn-prev"><img src="./images/arrow-left.svg"/></span>`,
-  nextArrow: `<span class="btn-arrow btn-prev"><img src="./images/arrow-right.svg"/></span>`,
+  nextArrow: `<span class="btn-arrow btn-next"><img src="./images/arrow-right.svg"/></span>`,
   infinite: true,
   fade: true,
+  asNavFor: ".feedback__main--nav",
+});
+
+$(".feedback__main--nav").slick({
+  slidesToShow: 5,
+  centerMode: true,
+  focusOnSelect: true,
+  asNavFor: ".feedback__main--box",
+  arrows: false,
+  dots: false,
+  centerPadding: 0,
 });
