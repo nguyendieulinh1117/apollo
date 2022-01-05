0239ab554454f470128461a5ea743c1e0d71f142
Vicky Nguyen <ztieu.linhz@gmail.com>
2022-01-05 11:44:19 +0700

    vision

--- js/app.js
+++ js/app.js
@@ -26,3 +26,32 @@ $(".feedback__main--nav").slick({
   dots: false,
   centerPadding: 0,
 });
+
+$(document).ready(function () {
+  if ($(".vision__main--left--item").length > 0) {
+    $(".vision__main--left--item .vision__title").on("click", function () {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active").next(".vision__content").slideUp();
+      } else {
+        $(".vision__main--left--item .vision__title").removeClass("active");
+        $(".vision__main--left--item .vision__content").slideUp();
+        $(this).toggleClass("active").next(".vision__content").slideToggle();
+      }
+      // if ($(this).hasClass("active")) {
+      //   $(this).removeClass("active").next(".vision__content").slideUp();
+      // } else {
+      //   $(".accordion .accordion-title").removeClass("active");
+      //   $(".accordion .accordion-content").slideUp();
+      //   $(this).toggleClass("active").next(".accordion-content").slideToggle();
+      // }
+      // var index_item = $(this).parents(".accordion").index();
+      // console.log(index_item);
+      // $(".wrapper-vision .vision-image .box-image").removeClass("active");
+      // $(".wrapper-vision .vision-image .box-image")
+      //   .eq(index_item)
+      //   .addClass("active");
+    });
+    $(".vision__main--left--item .vision__title").first().trigger("click");
+    // $(".wrapper-vision .vision-image .box-image").first().addClass("active");
+  }
+});
